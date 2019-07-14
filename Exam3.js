00547750de71d2d3534703b8054505e878591c32
Caominh123 <caoliem1609@gmail.com>
2019-07-14 09:22:59 +0700

    update with code pretier

--- Exam3.js
+++ Exam3.js
@@ -1,24 +1,26 @@
-var a = "This is a sentence 48ascd with 12 and se789 and lk890kl end";
-var b = _.split(a," ");//cat a ra thanh mang cac phan tu
+var a = "This is a sentence 48ascd with 12 and se789 and lk890kl end";
+var b = _.split(a, " ");
 var c = _.clone(b);
-    for (var x of b) {
-        var i = 0, f = false;
-        while(i < x.length -1){
-            if((_.toInteger(x[i])>0 && _.toInteger(x[i+1])==0)||(_.toInteger(x[i]) == 0 && _.toInteger(x[i+1])>0)) {
-                f=true;
-                break;     
-            } else {
-                 i++;
-                }
-        }
-        if(f==false)
-            var senc = _.pull(c,x);    
-        }
-        
- console.log("Matchted words: " +"'"+ c+ "'");
- var d = [];
- for (var x of c) {
-    
-    d.push(_.kebabCase(x));
- }
- console.log("Variation with '-' character: " + "'" +  d + "'" );
+for (var x of b) {
+  var i = 0,
+    f = false;
+  while (i < x.length - 1) {
+    if (
+      (_.toInteger(x[i]) > 0 && _.toInteger(x[i + 1]) == 0) ||
+      (_.toInteger(x[i]) == 0 && _.toInteger(x[i + 1]) > 0)
+    ) {
+      f = true;
+      break;
+    } else {
+      i++;
+    }
+  }
+  if (f == false) var senc = _.pull(c, x);
+}
+
+console.log("Matchted words: " + "'" + c + "'");
+var d = [];
+for (var x of c) {
+  d.push(_.kebabCase(x));
+}
+console.log("Variation with '-' character: " + "'" + d + "'");
