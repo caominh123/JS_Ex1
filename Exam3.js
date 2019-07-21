f44a8b77dc34ac6a375995f55429f53eaa11bd30
= <=>
2019-07-21 15:28:47 +0700

    adjust with ES6

--- Exam3.js
+++ Exam3.js
@@ -1,26 +1,25 @@
-var a = "This is a sentence 48ascd with 12 and se789 and lk890kl end";
-var b = _.split(a, " ");
-var c = _.clone(b);
-for (var x of b) {
-  var i = 0,
-    f = false;
+let _ = require('lodash');
+let a = "This is a sentence 48ascd with 12 and se789 and lk890kl end";
+let b = _.split(a, " ");
+let c = _.clone(b);
+for (let x of b) {
+  let i = 0, f = false;
   while (i < x.length - 1) {
-    if (
-      (_.toInteger(x[i]) > 0 && _.toInteger(x[i + 1]) == 0) ||
-      (_.toInteger(x[i]) == 0 && _.toInteger(x[i + 1]) > 0)
-    ) {
+    if ((_.toInteger(x[i]) > 0 && _.toInteger(x[i + 1]) == 0) || (_.toInteger(x[i]) == 0 && _.toInteger(x[i + 1]) > 0)) {
       f = true;
       break;
     } else {
       i++;
     }
   }
-  if (f == false) var senc = _.pull(c, x);
+  if (f == false)
+    _.pull(c, x);
 }
 
-console.log("Matchted words: " + "'" + c + "'");
-var d = [];
-for (var x of c) {
+console.log(`Matchted words: '${c}'`);
+let d = [];
+for (let x of c) {
+
   d.push(_.kebabCase(x));
 }
-console.log("Variation with '-' character: " + "'" + d + "'");
+console.log(`Variation with '-' character: '${d}'`);
